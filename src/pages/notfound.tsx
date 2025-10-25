a7eef7584a6a614a57760898a4c0312a6f1cf66d
RuiBabosaJ <ruibarbosadev@gmail.com>
2025-10-25 14:55:25 -0300

    style(notfound): adicionando style a pagina

--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -4,11 +4,14 @@ import { Button } from "../components/button";
 export function NotDefound() {
   return (
     <>
-      <div className="flex items-center w-full justify-center h-screen text-white">
-        Pagina não encontrada!
-        <Link to={"/"} className="px-3">
-          <Button className="">Volte</Button>
-        </Link>
+      <div className="flex items-center flex-col w-full justify-center h-screen text-white gap-2">
+        <h1 className="font-extrabold text-7xl">404</h1>
+        <h3 className="text-3xl">Pagina não encontrada!</h3>
+        <nav>
+          <Link to={"/"}>
+            <Button className="px-10 h-7">Volte</Button>
+          </Link>
+        </nav>
       </div>
     </>
   );
